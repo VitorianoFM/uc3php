ce0ec3fda1bb92db7f0218835dc0ae109aaea882
Vitoriano Ferrero Martin Jr <victorianojr@gmail.com>
2025-03-13 16:44:42 -0300

    Atividade 28 adicionado
    Atividade 28 adicionado

--- htdocs/Vitoriano/Atividades/exercicio28.php
+++ htdocs/Vitoriano/Atividades/exercicio28.php
@@ -1,4 +1,9 @@
 <?php
 
-// 
+// Verificação se o número é par
 
+echo "Digite um número: ";
+$numero = trim(fgets(STDIN));
+$numeroDois = 2;
+
+echo ($numero % $numeroDois ? "O número não é par" : "O número é par")."\n";
